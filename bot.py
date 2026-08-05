a0bf2063721b9e8f1029d18890a18c005a61c008
shoakbaratxamov-cyber <shoakbaratxamov@gmail.com>
2026-08-05 18:25:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 
 # BotFather'dan olgan tokeningiz
-BOT_TOKEN = "8942899247:AAHimpQuuvopoDFUweQXITUQSYCpdrAAtLQ"
+BOT_TOKEN = "BOT_TOKEN"
 
 # Ombor.xlsx faylining nomi
 EXCEL_FILE = "Ombor.xlsx"
